58aaa6a57f8bb5feb0059c8bad5b20a2fd144bd3
Adhy <adhywiranatap@gmail.com>
2017-04-03 09:31:13 +0700

    added fetch function to post actionCreator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,13 @@
 import * as ActionTypes from './actionTypes';
 
 export const fetchPostsSuccess = (posts) => ({
-  type: ActionTypes.FETCH_POSTS,
+  type: ActionTypes.FETCH_POSTS_SUCCESS,
   payload: posts,
 });
-export const fetchPosts = () => {
-  return (dispatch) => {
-    // fetch('')
-  }
+export const fetchPosts = () => (dispatch) => {
+  setTimeout(() =>
+    fetch('http://localhost:1234/posts')
+      .then(res => res.json())
+      .then(posts => dispatch(fetchPostsSuccess(posts)))
+  , 2000);
 };

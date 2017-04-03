44bb39b072d26aabb4063b2b49050b89851d9fd8
Adhy <adhywiranatap@gmail.com>
2017-04-03 09:47:38 +0700

    modularized Header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+
+const Header = () => (
+  <div className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h2>REACT REDUX TYPICAL BLOG</h2>
+  </div>
+);
+
+export default Header;

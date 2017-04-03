61d42c46ff4331e35dc79733027650c0bfa6d91f
Adhy <adhywiranatap@gmail.com>
2017-04-03 20:40:30 +0700

    updated post author for schema

--- src/schemas/post.js
+++ src/schemas/post.js
@@ -1,7 +1,8 @@
 import { schema } from 'normalizr';
 
-const comment = new schema.Entity('comments');
-const post = new schema.Entity('posts', { comments: [comment] });
+const user = new schema.Entity('users');
+const comment = new schema.Entity('comments', { commenter: user });
+const post = new schema.Entity('posts', { author: user, comments: [comment] });
 const postSchema = { posts: [post] };
 
 export default postSchema;

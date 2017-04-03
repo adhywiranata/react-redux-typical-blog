c4e7da8a0e4ad68c2dea3e56647d3dcf3e0862be
Adhy <adhywiranatap@gmail.com>
2017-04-03 09:48:23 +0700

    modularized components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
-import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
-import RefreshIndicator from 'material-ui/RefreshIndicator';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
-injectTapEventPlugin();
-
 import { fetchPosts } from './actions';
-import logo from './logo.svg';
 import './App.css';
+import { Header, LoadingSpinner, PostItemCard } from './components';
+
+injectTapEventPlugin();
 
 class App extends Component {
   componentDidMount() {
@@ -21,56 +18,18 @@ class App extends Component {
     return (
       <MuiThemeProvider>
         <div className="App">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>REACT REDUX TYPICAL BLOG</h2>
-          </div>
+          <Header />
           { this.props.posts.length === 0 && (<div>
             <LoadingSpinner />
           </div>)}
           <div style={{ width: '50%', marginLeft: '25%' }}>
-            { this.props.posts.map((post, index) => <CardExampleWithAvatar key={index} />) }
+            { this.props.posts.map(post => <PostItemCard key={post._id} {...post} />) }
           </div>
         </div>
       </MuiThemeProvider>
     );
   }
 }
-
-const LoadingSpinner = () => (
-  <RefreshIndicator
-    size={50}
-    left={0}
-    top={20}
-    loadingColor="#FF9800"
-    status="loading"
-    style={{ display: 'inline-block', position: 'relative' }}
-  />
-);
-
-const CardExampleWithAvatar = () => (
-  <Card style={{ marginTop: 50, paddingBottom: 20 }}>
-    <CardHeader
-      title="Dave Johnson"
-      subtitle="Scientist"
-      avatar="http://www.material-ui.com/images/jsa-128.jpg"
-      style={{ float: 'left', textAlign: 'left' }}
-    />
-    <CardMedia>
-      <img alt="space" src="http://www.deepspacephotography.com/wp-content/gallery/cache/203__1096x_headerimage_5.jpg" />
-    </CardMedia>
-    <CardTitle title="Art of Andromeda" subtitle="The Beauty of Outer Space" />
-    <CardText>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-      Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-      Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-    </CardText>
-    <CardActions>
-      <RaisedButton label="Read More" primary />
-    </CardActions>
-  </Card>
-);
 
 const mapStateToProps = state => ({
   posts: state.posts,
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Header from './Header';
+import LoadingSpinner from './LoadingSpinner';
+import PostItemCard from './PostItemCard';
+
+export { Header, LoadingSpinner, PostItemCard };

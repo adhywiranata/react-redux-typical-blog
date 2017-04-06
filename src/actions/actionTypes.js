f1d3de7b9a229a8b68ceb5d13c503b37a80122e9
Adhy <adhywiranatap@gmail.com>
2017-04-06 11:14:19 +0700

    added actionTypes for searchkey

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,2 +1,5 @@
 export const FETCH_POSTS_SUCCESS = 'FETCH_POSTS_SUCCESS';
 export const FETCH_POSTS = 'FETCH_POSTS';
+
+export const SET_SEARCH_KEY = 'SET_SEARCH_KEY';
+export const RESET_SEARCH_KEY = 'RESET_SEARCH_KEY';

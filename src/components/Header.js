87ac09f10d5503ba4e03ba08c3c219d954b62d75
Adhy <adhywiranatap@gmail.com>
2017-04-06 11:07:03 +0700

    added searchbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,11 @@ const Header = () => (
   <div className="App-header">
     <img src={logo} className="App-logo" alt="logo" />
     <h2>REACT-REDUX TYPICAL BLOG</h2>
+    <input
+      type="text"
+      placeholder="Search for awesome news..."
+      style={{ width: '50%', margin: 10, padding: 10, fontSize: 20, outline: 'none' }}
+    />
   </div>
 );
 

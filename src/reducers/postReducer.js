8ccb598308a6f5c83fb5ea632966ce87c881e8d4
Adhy <adhywiranatap@gmail.com>
2017-04-06 14:27:15 +0700

    removed selector from reducer

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,6 +1,3 @@
-import { denormalize } from 'normalizr';
-import postSchema from '../schemas/post';
-
 import * as ActionTypes from '../actions/actionTypes';
 
 const postReducer = (state = {}, action) => {
@@ -13,10 +10,3 @@ const postReducer = (state = {}, action) => {
 };
 
 export default postReducer;
-
-// Selectors
-export const getFilteredPost = (posts, searchKey) => {
-  const result = posts.result === undefined ? { posts: [] } : posts.result;
-  const denormalizedPost = denormalize(result, postSchema, posts.entities);
-  return denormalizedPost.posts.filter(post => post.title.toLowerCase().includes(searchKey));
-};

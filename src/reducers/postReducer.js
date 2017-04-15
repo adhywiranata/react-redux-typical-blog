c6619833dffeb84bcd5b4ec8be27ce3524998b16
Adhy <adhywiranatap@gmail.com>
2017-04-15 19:50:25 +0700

    modified initial state structure

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -2,13 +2,18 @@ import * as ActionTypes from '../actions/actionTypes';
 
 const initialState = {
   isFething: false,
+  isFetchingError: false,
   data: [],
 };
 
 const postReducer = (state = initialState, action) => {
   switch (action.type) {
     case ActionTypes.FETCH_POSTS_SUCCESS:
-      return { isFetching: false, data: action.payload };
+      return {
+        isFetching: false,
+        isFetchingError: false,
+        data: action.payload
+      };
     default:
       return state;
   }

07a3a444c677f2472fa975d00bd481acb22965ca
Adhy <adhywiranatap@gmail.com>
2017-04-15 19:54:22 +0700

    added post fetch request state

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,18 +1,20 @@
 import * as ActionTypes from '../actions/actionTypes';
 
 const initialState = {
-  isFething: false,
+  isFetching: false,
   isFetchingError: false,
   data: [],
 };
 
 const postReducer = (state = initialState, action) => {
   switch (action.type) {
+    case ActionTypes.FETCH_POSTS_REQUEST:
+      return { ...state, isFetching: true };
     case ActionTypes.FETCH_POSTS_SUCCESS:
       return {
         isFetching: false,
         isFetchingError: false,
-        data: action.payload
+        data: action.payload,
       };
     default:
       return state;
